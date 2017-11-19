b18aaf4e59f8bd7df0de8c55fc111ada5b8ac9bd
johnnychan <captainchentong@sina.com>
2017-11-19 22:51:19 +0800

    spring-boot test

---
+++ src/main/java/com/johnnychan/spring/demo/Example.java
@@ -0,0 +1,23 @@
+package com.johnnychan.spring.demo;
+
+/**
+ * Created by lenovo on 2017/11/5.
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
